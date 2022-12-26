de693e07ff412ae714043db174c532b3e2e8e23e
VivianYueh <100284513+VivianYueh@users.noreply.github.com>
2022-12-26 23:45:29 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config ={
     app:{port:3000
     },
     db:{
-        url:"mongodb+srv://VivianYueh:<password>@scoreboard.usvcnmw.mongodb.net/?retryWrites=true&w=majority"
+        url:"mongodb+srv://VivianYueh:VivianYueh@scoreboard.usvcnmw.mongodb.net/?retryWrites=true&w=majority"
     }
 };
 module.exports=config;
